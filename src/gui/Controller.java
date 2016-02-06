837cc75101e25f615370e9211d59ae78c0dfc1db
Brian Patino <brian.patino@uconn.edu>
2016-02-05 23:04:43 -0500

    Create Directory Tree View * bind top menu to application resize * bind tree view to application resize

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,30 @@
 package gui;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+import java.util.ResourceBundle;
+
+public class Controller{
+    @FXML
+    private MenuItem menuOpen;
+
+    @FXML private ResourceBundle resources;
+
+    /*@FXML
+    private void initialize() {
+        menuOpen.setOnAction(openMenu());
+    }
+
+    private EventHandler<ActionEvent> openMenu() {
+        EventHandler<ActionEvent> evenHand = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                menuOpen.setText("Somewhat right...");
+            }
+        };
+        return evenHand;
+    }*/
+
 }
+
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -10,13 +10,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+//        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+       /* Button btn = new Button();
+        btn.setText("This is a test");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.print("Bingo you did something!");
+            }
+        });*/
         Parent root = FXMLLoader.load(getClass().getResource("StartPage.fxml"));
         primaryStage.setTitle("Iron-gate!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 788, 576));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
