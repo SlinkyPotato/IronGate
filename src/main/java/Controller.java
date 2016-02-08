a50a4a5eafff9145e98860a7e4fe5df8a752f52a
Brian Patino <brian.patino@uconn.edu>
2016-02-07 21:59:33 -0500

    Create Disk View * add sample disk view * attach controller to StartPage.fxml

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,9 +2,16 @@ package main.java;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.util.Callback;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class Controller{
@@ -13,23 +20,36 @@ public class Controller{
     @FXML private MenuItem fileTag;
     @FXML private MenuItem fileExit;
     @FXML private MenuItem editPreferences;
-    @FXML private TreeView<String> dirTree;
+    @FXML private TreeView<File> dirTree;
 
     @FXML private ResourceBundle resources;
 
-    @FXML
-    private void initialize() {
-//        createTree();
+    @FXML private void initialize() {
+        createTree();
     }
 
     // Create the directory Tree
     private void createTree() {
-        TreeItem<String> root = new TreeItem<String>("Root");
-        TreeItem<String> itemChild = new TreeItem<String>("Child");
-        itemChild.setExpanded(false);
-        root.getChildren().add(itemChild);
-        dirTree.setRoot(root);
+        dirTree.setRoot(new TreeItem<>());
+        dirTree.setShowRoot(false);
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            TreeItem<File> itemChild = new TreeItem<>(root);
+            itemChild.setExpanded(false);
+            dirTree.getRoot().getChildren().add(itemChild);
+        }
+        /*dirTree.setCellFactory(new Callback<TreeView<File>, TreeCell<File>>() {
+            @Override
+            public TreeCell<File> call(TreeView<File> param) {
+                return new TreeFieldImpl();
+            }
+        });*/
     }
 
+    private class TreeFieldImpl extends TreeCell<File> {
+        public TreeFieldImpl() {
+//            MenuItem
+        }
+    }
 }
 
