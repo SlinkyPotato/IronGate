d09c95919f687e90d065df7a6e31254b3617ab78
Brian Paitno <patinobrian@gmail.com>
2016-02-25 20:40:28 -0500

    Dropbox Tests * Add Dropbox Controller * Add Dropbox tests * Add Dropbox menu item

--- src/main/java/launcher/Controller.java
+++ src/main/java/launcher/Controller.java
@@ -1,8 +1,13 @@
-package directory;
+package launcher;
 
+import directory.FolderViewManager;
+import directory.IronFile;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeView;
+import com.dropbox.core.*;
+import com.dropbox.core.v2.*;
+import webapp.DropboxController;
 
 import java.util.ResourceBundle;
 
@@ -12,13 +17,22 @@ public class Controller{
     @FXML private MenuItem fileTag;
     @FXML private MenuItem fileExit;
     @FXML private MenuItem editPreferences;
+    @FXML private MenuItem fileDropboxSignin;
     @FXML private TreeView<IronFile> dirTree;
     @FXML private ResourceBundle resources;
+
+
 
     @FXML private void initialize() {
         FolderViewManager manager = new FolderViewManager(dirTree); // 2 statements in 1 line is best
         IronFile[] hardDrives = IronFile.listRoots(); // an array of hard drives
         manager.setRootDirectory(hardDrives);
+        DropboxController dbManager = new DropboxController();
+        try {
+            dbManager.test();
+        } catch (DbxException e) {
+            e.printStackTrace();
+        }
 //        directory.IronFile homeDir = new directory.IronFile(System.getProperty("user.home")); // use this for specific directory
 //        manager.setRootDirectory(homeDir);
     }
--- src/main/java/webapp/Dropbox.java
+++ src/main/java/webapp/Dropbox.java
@@ -1,27 +1,63 @@
 package webapp;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 
 /**
  * Dropbox API specific
  */
 public class Dropbox {
     // retrieve private credentials from Dropbox Developers app
-    static {
-        try {
-            FileReader readCred = new FileReader("dropbox.json");
-            JSONObject dropCred = new JSONObject(readCred);
-            final String key = dropCred.getString("key");
-            final String secret = dropCred.getString("secret");
-            final String token = dropCred.getString("token");
+    private String key;
+    private String secret;
+    private String token;
 
+    public Dropbox() {
+        readSetCredentials();
+    }
+    public Dropbox(String key, String secret) {
+        this.key = key;
+        this.secret = secret;
+    }
+    public Dropbox(String key, String secret, String token) {
+        this.key = key;
+        this.secret = secret;
+        this.token = token;
+    }
+
+    private void readSetCredentials() {
+        try {
+            URL url = getClass().getResource("/services/dropbox.json");
+            FileReader readCred = new FileReader(url.getPath());
+            JSONObject dropCred = new JSONObject(url.getPath());
+            key = dropCred.getString("key");
+            secret = dropCred.getString("secret");
+            token = dropCred.getString("token");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String getKey() {
+        return key;
+    }
+    public String getSecret() {
+        return secret;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+    public void setToken(String token) {
+        this.token = token;
     }
 
 
