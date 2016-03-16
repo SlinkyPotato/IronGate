af4ff77c97a24926986e81e31cba5936d4e5e2c3
Kristopher Guzman <kristopherguz@gmail.com>
2016-03-16 14:45:15 -0400

    a

--- src/main/java/directory/FileViewTreeCell.java
+++ src/main/java/directory/FileViewTreeCell.java
@@ -1,22 +1,55 @@
 package directory;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 
 /**
- * @author kristopherguzman
+ * Created by kristopherguzman on 3/10/16.
  */
 public class FileViewTreeCell extends TreeCell<IronFile> {
-    public FileViewTreeCell(TreeView<IronFile> treeView) {}
+
+    public FileViewTreeCell(TreeView<IronFile> treeView) {
+
+        setOnDragEntered(args -> {
+            if(args.getGestureSource().getClass().getName().equals("String")) {
+                setStyle("-fx-background-color: aqua;");
+            }
+            args.consume();
+        });
+
+        setOnDragExited(args -> {
+            setStyle("");
+            args.consume();
+        });
+
+        setOnDragOver(args -> {
+            if(args.getGestureSource().getClass().getName().equals("String")) {
+                args.acceptTransferModes(TransferMode.COPY);
+            }
+            args.consume();
+        });
+
+        setOnDragDropped(args -> {
+
+        });
+    }
 
     @Override
     protected void updateItem(IronFile item, boolean empty) {
         if(item == getItem()) return;
+
         super.updateItem(item, empty);
         if(item == null) {
             super.setText(null);
         } else {
             super.setText(item.getName());
+
         }
     }
 }
